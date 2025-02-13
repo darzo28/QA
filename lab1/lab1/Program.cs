eaaa68b7957230037c2515bda24d5835b97e4244
darzo28 <darzo28@yandex.ru>
2025-02-13 13:34:01 +0300

    fix tests

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -2,59 +2,60 @@
 using System.Globalization;
 
 namespace lab1
+{
+    public class Program
     {
-        public class Program
+        private static string GetTriangleType(double a, double b, double c)
         {
-            private static string GetTriangleType(double a, double b, double c)
+            if (a + b <= c || a + c <= b || b + c <= a)
             {
-                if (a + b <= c || a + c <= b || b + c <= a || a < 0 || b < 0 || c < 0)
-                {
-                    return "не треугольник";
-                }
-                else if (a != b && a != c && b != c)
-                {
-                    return "обычный";
-                }
-                else if (a == b && b == c)
-                {
-                    return "равносторонний";
-                }
-                else
-                {
-                    return "равнобедренный";
-                }
+                return "не треугольник";
             }
-
-            public static void Main(string[] args)
+            else if (a != b && a != c && b != c)
             {
-                const string error = "неизвестная ошибка";
+                return "обычный";
+            }
+            else if (a == b && b == c)
+            {
+                return "равносторонний";
+            }
+            else
+            {
+                return "равнобедренный";
+            }
+        }
 
-                if (args.Length != 3)
-                {
-                    Console.Write(error);
-                }
-                else
-                {
-                    try
-                    {
-                        double a = double.Parse(args[0]);
-                        double b = double.Parse(args[1]);
-                        double c = double.Parse(args[2]);
+        public static void Main(string[] args)
+        {
+            const string error = "неизвестная ошибка";
 
-                        if (double.IsInfinity(a) || double.IsInfinity(b) || double.IsInfinity(c))
-                        {
-                            Console.Write(error);
-                        }
-                        else
-                        {
-                            Console.Write(GetTriangleType(a, b, c));
-                        }
-                    }
-                    catch (FormatException)
+            if (args.Length != 3)
+            {
+                Console.Write(error);
+            }
+            else
+            {
+                try
+                {
+                    double a = double.Parse(args[0]);
+                    double b = double.Parse(args[1]);
+                    double c = double.Parse(args[2]);
+
+                    if (double.IsInfinity(a + b) || double.IsInfinity(b + c) || double.IsInfinity(a + c) || 
+                        a < 0 || b < 0 || c < 0)
                     {
                         Console.Write(error);
                     }
+                    else
+                    {
+                        Console.Write(GetTriangleType(a, b, c));
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.Write(error);
                 }
             }
         }
     }
+}
